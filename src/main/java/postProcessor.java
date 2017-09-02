9c3c84392f890575721ccaa2c867809d817f2606
vonclaude <colinfujii@gmail.com>
2017-09-01 21:47:30 -0700

    Re-added full list feature, added exception handling
    Re-enabled the ability to output the entire list of actions at once.
    Also, program now checks if the thread URL input exists before moving
    onto next step.

--- src/main/java/postProcessor.java
+++ src/main/java/postProcessor.java
@@ -1,9 +1,12 @@
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
 import java.util.*;
 
 /**
@@ -13,28 +16,56 @@ public class postProcessor {
 
     public static void main(String [ ] args) throws IOException {
         postProcessor postProcessor = new postProcessor();
-        ArrayList<String> nameList = new ArrayList<String>();
-        ArrayList<String> diceList = new ArrayList<String>();
-        ArrayList<ArrayList<String>> actionList = new ArrayList<ArrayList<String>>();
 
-        //Ask user for thread URL
-        Scanner threadURLScanner = new Scanner(System.in);
-        System.out.println("Enter the URL of the thread: ");
-        String threadURL = threadURLScanner.next();
+        ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<String> diceList = new ArrayList<>();
+        ArrayList<ArrayList<String>> actionList = new ArrayList<>();
+        Document doc = null;
+        String threadURL;
+        int phaseStart;
+        boolean outputAsList = false;
+        boolean validThreadInput = false;
+        boolean validBooleanInput = false;
+
+        //Ask user for thread URL, check that it exists
+        do {
+            try {
+                System.out.print("Enter the URL of the thread: ");
+                Scanner threadURLScanner = new Scanner(System.in);
+                threadURL = threadURLScanner.next();
+                doc = Jsoup.connect(threadURL)
+                        .maxBodySize(0)
+                        .timeout(600000)
+                        .get();
+                validThreadInput = true;
+
+            } catch (UnknownHostException | HttpStatusException | MalformedURLException ex) {
+                System.out.println("Invalid URL!");
+            }
+        } while (!validThreadInput);
 
         //Ask user for post that starts Action Phase
         Scanner phaseStartScanner = new Scanner(System.in);
         System.out.print("Enter the post number of the start of the action phase: ");
-        int phaseStart = phaseStartScanner.nextInt();
+        phaseStart = phaseStartScanner.nextInt();
 
-        //Connect to thread, parse HTML for list of posts
-        Document doc = Jsoup.connect(threadURL)
-                .maxBodySize(0)
-                .timeout(600000)
-                .get();
+        //Ask user for full list output or per post output
+        do {
+            try {
+                System.out.print("Type true for the full list of actions, false for one at a time.");
+                Scanner postOutputScanner = new Scanner(System.in);
+                outputAsList = postOutputScanner.nextBoolean();
+                validBooleanInput = true;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Input true or false exactly as shown!");
+            }
+        } while (!validBooleanInput);
+
+        //Parse DOM for list of posts
         Elements postList = doc.select(".post");
 
-        //Output
+        //Output relevant posts
         for (Element post : postList) {
             int postNum = Integer.parseInt(post.attr("id").substring(1));
 
@@ -44,7 +75,7 @@ public class postProcessor {
                 String dice = postProcessor.getDice(post);
                 ArrayList actions = postProcessor.getActions(post);
 
-                //Filters anonymous posts, posts with no Greentext
+                //Filters anonymous posts, posts with no greentext
                 if (!name.equals("Anonymous") && !actions.isEmpty()) {
                     nameList.add(name);
                     diceList.add(dice);
@@ -57,19 +88,30 @@ public class postProcessor {
         for(int i = 0; i < nameList.size(); i++) {
             ArrayList currentActions = actionList.get(i);
 
+            System.out.println("=====");
             System.out.println(nameList.get(i));
 
             if(!"".equals(diceList.get(i))) {
                 System.out.println(diceList.get(i));
             }
 
-            for (int j = 0; j < currentActions.size(); j++) {
-                System.out.println(currentActions.get(j));
+            for (Object currentAction : currentActions) {
+                System.out.println(currentAction);
             }
 
-            Scanner scanner = new Scanner(System.in);
-            scanner.nextLine();
+            if(!outputAsList) {
+                Scanner scanner = new Scanner(System.in);
+                scanner.nextLine();
+            } else {
+                System.out.println();
+            }
 
+        }
+
+        if(outputAsList) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Press Enter to close console.");
+            scanner.nextLine();
         }
     }
 
@@ -84,7 +126,7 @@ public class postProcessor {
     }
 
     private ArrayList getActions(Element post) {
-        ArrayList<String> actionList = new ArrayList<String>();
+        ArrayList<String> actionList = new ArrayList<>();
         Elements actions = post.select(".quote");
 
         for (Element action : actions) {
